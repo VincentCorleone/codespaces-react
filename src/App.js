c54aa1c1bec713187b518a0e6150881924a80ca9
Vincent Ge <707119577@qq.com>
2022-12-18 11:49:33 +0000

    add component Square

--- src/App.js
+++ src/App.js
@@ -1,4 +1,49 @@
+import React from 'react';
 import './App.css';
+
+class Board extends React.Component {
+  renderSquare(i){
+    return <Square value={i} />
+  }
+
+  
+  render(){
+    const status = "Next player: X"
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div>
+          <div>{this.renderSquare(1)}</div>
+          <div>{this.renderSquare(2)}</div>
+          <div>{this.renderSquare(3)}</div>
+        </div>
+
+        <div>
+          <div>{this.renderSquare(4)}</div>
+          <div>{this.renderSquare(5)}</div>
+          <div>{this.renderSquare(6)}</div>
+        </div>
+
+        <div>
+          <div>{this.renderSquare(7)}</div>
+          <div>{this.renderSquare(8)}</div>
+          <div>{this.renderSquare(9)}</div>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+class Square extends React.Component {
+  render(){
+    return (
+      <button>
+        {this.props.value}
+      </button>
+    )
+  }
+}
 
 function App() {
   return (
@@ -12,13 +57,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
             Learn React
+            <Board></Board>
           </a>
         </p>
       </header>
